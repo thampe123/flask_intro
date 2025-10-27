cc645b9e86447db76c37662a6363e820f6d7792a
tdrave <thomas.derave@ugent.be>
2025-10-27 17:43:36 +0100

    Update database URI for local development

--- app/config.py
+++ app/config.py
@@ -1,4 +1,4 @@
 class Config: 
     SECRET_KEY = 'your_secret_key'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres.infrcdwltwixqcxcfshc:booksMPpasswordx@aws-0-us-east-1.pooler.supabase.com:6543/postgres'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres.username:password@localhost:port/dbname'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
